3df4ec00331771592ab8109260e1dace0a303cb6
Faizaan Charania <faizaancharania@gmail.com>
2017-12-02 20:48:55 -0500

    work at midway progress | working on new error metric

--- Project/debug.py
+++ Project/debug.py
@@ -16,44 +16,45 @@ from sklearn import metrics
 from math import log
 
 
-# import pdb; pdb.set_trace()
-def eval_probs(y_prob,y_test, labels):
-	"""
-	y_prob : predict_proba output of a model
-	labels : all unique labels in the classification
-	"""
-	vals = []
-	titles = {}
-	titles[-1] = "Away Win"
-	titles[0] = "Draw"
-	titles[1] = "Home Win"
+import pdb; pdb.set_trace()
+def eval_probs(y_prob,y_test, labels=[-1,0,1],show_plot=False):
+    """
+    y_prob : predict_proba output of a model
+    labels : all unique labels in the classification
+    """
+    vals = []
+    titles = {}
+    titles[-1] = "Away Win"
+    titles[0] = "Draw"
+    titles[1] = "Home Win"
 
-	y_prob = pd.DataFrame(y_prob, columns=[-1, 0, 1])
-	y_prob = y_prob.round(1)
-	y_prob['label'] = y_test.reset_index(drop=True)
-	slopes = []
-	for ind, label in enumerate(labels):
-		vals = sorted(y_prob[label].unique())
-		
-		y_col = []
-		
-		
-		for i in vals:
-			tot_len = y_prob[(y_prob[label] == i)].shape[0]
-			matching_len = y_prob[(y_prob[label] == i) & (y_prob['label'] == label)].shape[0]
-			y_col.append(matching_len*1.0/tot_len)
-		
-			# p = np.polyfit(y_prob[(y_prob[label] == i)],y_col,deg=1)
-			# print p
-		slopes.append(np.polyfit(vals,y_col,deg=1)[0])
-#         plt.figure(figsize=(10,16))
-#         plt.subplot(311+ind)
-#         plt.bar(vals, y_col, width=0.05)
-#         plt.title(titles[label], fontsize=16)
-#         plt.xticks(np.arange(0.0, 1.1, 0.1), fontsize=15)
-#         plt.yticks(fontsize=15)
-#         plt.show()
-	print mean_squared_error([1,1,1],slopes)**0.5
+    y_prob = pd.DataFrame(y_prob, columns=[-1, 0, 1])
+    # y_prob = y_prob.round(1)
+    y_prob['label'] = y_test.reset_index(drop=True)
+    slopes= []
+    
+    for ind, label in enumerate(labels):
+        vals = sorted(y_prob[label].unique())
+        
+        y_col = []
+        
+        
+        for i in vals:
+            tot_len = y_prob[(y_prob[label] == i)].shape[0]
+            matching_len = y_prob[(y_prob[label] == i) & (y_prob['label'] == label)].shape[0]
+            y_col.append(matching_len*1.0/tot_len)
+        
+        slopes.append(np.polyfit(vals,y_col,deg=1)[0])    
+
+        if show_plot:
+            plt.figure(figsize=(7,21))
+            plt.subplot(311+ind)
+            plt.bar(vals, y_col, width=0.05)
+            plt.title(titles[label], fontsize=16)
+            plt.xticks(np.arange(0.0, 1.1, 0.1), fontsize=15)
+            plt.yticks(np.arange(0.0, 1.1, 0.1),fontsize=15)
+            plt.show()
+    return mean_squared_error([1,1,1],slopes)**0.5
 	
 def save_model(model, model_name):
 	saved_models_root = "./saved_models/" 
@@ -64,31 +65,39 @@ def save_model(model, model_name):
 
 
 def main():
-	with open("./data/train/train_data.pkl") as infile:
-		train_data = pkl.load(infile)
+	# with open("./data/train/train_data.pkl") as infile:
+	# 	train_data = pkl.load(infile)
 
-	columns = ['date', 'B365H', 'B365D', 'B365A', 'match_id', 'home_team', 'away_team', 
-			'winner', 'minute', 'H_Goal', 'A_Goal']
-	train_data = train_data[columns]
+	# columns = ['date', 'B365H', 'B365D', 'B365A', 'match_id', 'home_team', 'away_team', 
+	# 		'winner', 'minute', 'H_Goal', 'A_Goal']
+	# train_data = train_data[columns]
 
-	train_data.drop_duplicates(inplace=True, keep='first')
+	# train_data.drop_duplicates(inplace=True, keep='first')
 
-	X = train_data.drop(['winner', 'date', 'match_id', 'home_team', 'away_team'], axis=1)
-	Y = train_data['winner']
+	# X = train_data.drop(['winner', 'date', 'match_id', 'home_team', 'away_team'], axis=1)
+	# Y = train_data['winner']
 
-	X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+	# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
 
-	lr = LogisticRegression(n_jobs=-1,multi_class='multinomial',solver='lbfgs')
-	lr.fit(X_train,y_train)
+	# lr = LogisticRegression(n_jobs=-1,multi_class='multinomial',solver='lbfgs')
+	# lr.fit(X_train,y_train)
 
-	y_pred = lr.predict(X_test)
+	# y_pred = lr.predict(X_test)
 
-	print metrics.accuracy_score(y_test, y_pred)
+	# print metrics.accuracy_score(y_test, y_pred)
 
-	y_prob = lr.predict_proba(X_test)
+	# y_prob = lr.predict_proba(X_test)
 
-	eval_probs(y_prob, y_test, [-1,0,1])
+	with open('y_prob.pkl') as f:
+		y_prob = pkl.load(f)
+
+	with open('y_test.pkl') as f:
+		y_test = pkl.load(f)
+
+	
+
+	print eval_probs(y_prob, y_test, [-1,0,1],show_plot=True)
 
 if __name__ == '__main__':
 	main()
